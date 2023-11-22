9d9ed25d070f5d91ae8c947ec20bd4460d359f4a
Szaloki <szalokipeterkrisztian@gmail.com>
2023-11-22 21:54:36 +0100

    equation missing

--- script.js
+++ script.js
@@ -127,7 +127,7 @@ function MakeAgents(team) {
         div.classList.add('agent');
         if (playerTeam == 'T') {
             div.classList.add('terrorist');
-        } else if(playerTeam = 'CT') {
+        } else if(playerTeam == 'CT') {
             div.classList.add('counterTerrorist');
         }
         div.id = `agent${i}`
